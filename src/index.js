55cc304cafb3be76b8bad1ede56454cd1738c81c
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-11-04 17:03:32 -0300

    config: ESlint, Prettier & Structure

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Engine: Hermes</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
