aed2d8bb50c232a19b62ac701524a7ec9bdc6881
Júnior de Oliveira <ellismarjr@hotmail.com>
2019-11-06 16:27:32 -0300

    fix: change colors order

--- src/components/Background/index.js
+++ src/components/Background/index.js
@@ -2,7 +2,7 @@ import LinearGradient from 'react-native-linear-gradient';
 import styled from 'styled-components/native';
 
 export default styled(LinearGradient).attrs({
-  colors: ['#39998e', '#2b4d59'],
+  colors: ['#2b4d59', '#39998e'],
 })`
   flex: 1;
 `;
